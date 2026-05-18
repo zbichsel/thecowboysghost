dcbdaf2c0a704e12fccca38fa029313ad0e25f26
Zackrey Bichsel <biksel09@gmail.com>
2026-05-18 16:09:39 -0500

    under construction

---
+++ src/components/About/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './about.css';
+
+function About() {
+    return (
+        <div>
+            <h1>The Cowboy's Ghost</h1>
+            <p>Under Construction</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import About from '../components/About/about';
+
+function Home() {
+    return (
+        <>
+        <About />
+        </>
+    );
+}
+
+export default Home;
